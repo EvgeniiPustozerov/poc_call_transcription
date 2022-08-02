b34e3899289ff83eaf298a7060a050ddee39ef19
Evgenii Pustozerov <evgenii.pustozerov@exposit.com>
2022-08-02 18:23:10 +0400

    Added requirements. Code refactoring.

--- Interface.py
+++ Interface.py
@@ -1,48 +1,70 @@
 import glob
 import os
 import random
-import shutil
-from pathlib import Path
 
+import soundfile as sf
 import streamlit as st
-from Model import prediction
+from pydub import AudioSegment
 
-st.title('Cyrillic handwritten OCR demo')
-st.subheader('This simple demo shows the possibilities of the Transformer deep learning architecture in the task of '
-             'automatic text recognition for cyrillic texts. It now works with single-line samples. You can randomly '
+from modules.diarization.nemo_diarization import diarization
+
+st.title('Call Transcription demo')
+st.subheader('This simple demo shows the possibilities of the ASR and NLP in the task of '
+             'automatic speech recognition and diarization. It works with mp3, ogg and wav files. You can randomly '
              'pickup a set of images from the built-in database or try uploading your own files.')
 
 if st.button('Try random samples from the database'):
-    folder = "data/sample/"
+    folder = "data/datasets/crema_d_diarization_chunks"
     os.makedirs(folder, exist_ok=True)
-    list_all_audio = glob.glob("data/dataset/*.png")
-    chosen_files = sorted(random.sample(list_all_audio, 3))
-    for f in glob.glob(folder + '*'):
-        os.remove(f)
-    for f in chosen_files:
-        path = shutil.copy2(f, folder)
-    for f in glob.glob(folder + '*'):
-        col1, col2 = st.columns(2)
-        with col1:
-            st.image(f)
-        with col2:
-            st.text(f)
+    list_all_audio = glob.glob("data/datasets/crema_d_diarization_chunks/*.wav")
+    chosen_files = sorted(random.sample(list_all_audio, 1))
+    file_name = os.path.basename(chosen_files[0]).split(".")[0]
+    audio_file = open(chosen_files[0], 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes)
+    f = sf.SoundFile(chosen_files[0])
+    st.write("Starting transcription. Estimated processing time: %0.1f seconds" % (f.frames / (f.samplerate * 5)))
+    result = diarization(chosen_files[0])
+    with open("info/transcripts/pred_rttms/" + file_name + ".txt") as f:
+        transcript = f.read()
+    st.write("Transcription completed.")
+    st.write("Number of speakers: %s" % result[file_name]["speaker_count"])
+    st.write("Sentences: %s" % len(result[file_name]["sentences"]))
+    st.write("Words: %s" % len(result[file_name]["words"]))
+    st.download_button(
+        label="Download audio transcript",
+        data=transcript,
+        file_name='transcript.txt',
+        mime='text/csv',
+    )
 
-    preds = prediction(folder)
-    print(preds)
-    st.write(preds)
-uploaded_file = st.file_uploader("Choose your image with a single line of Russian text",
-                                 accept_multiple_files=False, type=["png", "jpeg", "jpg"])
+uploaded_file = st.file_uploader("Choose your recording with a speech",
+                                 accept_multiple_files=False, type=["mp3", "wav", "ogg"])
 if uploaded_file is not None:
     folder = "data/user_data/"
     os.makedirs(folder, exist_ok=True)
     for f in glob.glob(folder + '*'):
         os.remove(f)
-    bytes_data = uploaded_file.read()
-    st.image(bytes_data)
-    save_path = Path(folder, uploaded_file.name)
-    with open(save_path, mode='wb') as w:
-        w.write(uploaded_file.getvalue())
-    preds = prediction(folder)
-    print(preds)
-    st.write(preds)
+    save_path = folder + uploaded_file.name
+    sound = AudioSegment.from_mp3(uploaded_file)
+    sound.export(save_path, format="wav", parameters=["-ac", "1"])
+    file_name = os.path.basename(save_path).split(".")[0]
+    audio_file = open(save_path, 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes)
+    f = sf.SoundFile(save_path)
+    st.write("Starting transcription. Estimated processing time: %0.0f minutes and %02.0f seconds"
+             % ((f.frames / (f.samplerate * 3) // 60), (f.frames / (f.samplerate * 3) % 60)))
+    result = diarization(save_path)
+    with open("info/transcripts/pred_rttms/" + file_name + ".txt") as f:
+        transcript = f.read()
+    st.write("Transcription completed.")
+    st.write("Number of speakers: %s" % result[file_name]["speaker_count"])
+    st.write("Sentences: %s" % len(result[file_name]["sentences"]))
+    st.write("Words: %s" % len(result[file_name]["words"]))
+    st.download_button(
+        label="Download audio transcript",
+        data=transcript,
+        file_name='transcript.txt',
+        mime='text/csv',
+    )
--- modules/diarization/nemo_diarization.py
+++ modules/diarization/nemo_diarization.py
@@ -1,27 +1,19 @@
 import os
 
-from nemo.collections.asr.models import ClusteringDiarizer
 from nemo.collections.asr.parts.utils.decoder_timestamps_utils import ASR_TIMESTAMPS
+from nemo.collections.asr.parts.utils.diarization_utils import ASR_DIAR_OFFLINE
 from omegaconf import OmegaConf
 from pyannote.audio import Pipeline
-from nemo.collections.asr.parts.utils.diarization_utils import ASR_DIAR_OFFLINE
 
 ROOT = os.getcwd()
-MODEL_CONFIG = "info/configs/diarization_nemo/offline_diarization.yaml"
-MODEL_CONFIG_2 = "info/configs/diarization_nemo/offline_diarization_asr.yaml"
-data_dir = os.path.join(ROOT, 'info/configs/diarization_nemo/')
+MODEL_CONFIG = "info/configs/offline_diarization_asr.yaml"
+data_dir = os.path.join(ROOT, 'info/configs/')
 os.makedirs(data_dir, exist_ok=True)
-output_dir = os.path.join(ROOT, 'info/configs/diarization_nemo/vad/')
-output_dir_2 = os.path.join(ROOT, 'info/configs/diarization_nemo/vad_2/')
+output_dir = os.path.join(ROOT, 'info/transcripts/')
 os.makedirs(output_dir, exist_ok=True)
-os.makedirs(output_dir_2, exist_ok=True)
 
 
-def rttm_to_diar_hyp():
-    pass
-
-
-def diarization(file_path, with_asr=False):
+def diarization(file_path):
     # Create a manifest for input with below format.
     # {'audio_filepath': /path/to/audio_file, 'offset': 0, 'duration':None, 'label': 'infer', 'text': '-',
     # 'num_speakers': None, 'rttm_filepath': /path/to/rttm/file, 'uem_filepath'='/path/to/uem/filepath'}
@@ -36,7 +28,7 @@ def diarization(file_path, with_asr=False):
         'rttm_filepath': None,
         'uem_filepath': None
     }
-    with open(os.path.join(ROOT, 'info/configs/diarization_nemo/manifests/', 'input_manifest.json'), 'w') as fp:
+    with open(os.path.join(data_dir, 'manifests/', 'input_manifest.json'), 'w') as fp:
         json.dump(meta, fp)
         fp.write('\n')
 
@@ -54,47 +46,31 @@ def diarization(file_path, with_asr=False):
         vad_json["label"] = "SPEECH"
         vad_json["uniq_id"] = initial_json["uri"]
         output_json.append(vad_json)
-    with open(os.path.join(ROOT, 'info/configs/diarization_nemo/manifests/', 'external_vad_manifest.json'), 'w') as f:
+    with open(os.path.join(data_dir, 'manifests/', 'external_vad_manifest.json'), 'w') as f:
         for item in output_json:
             f.write(str(item).replace("'", '"') + '\n')
 
-    if with_asr:
-        config2 = OmegaConf.load(MODEL_CONFIG_2)
-        config2.diarizer.asr.model_path = 'QuartzNet15x5Base-En'
-        config2.diarizer.manifest_filepath = \
-            os.path.join(ROOT, 'info/configs/diarization_nemo/manifests/', 'input_manifest.json')
-        config2.diarizer.speaker_embeddings.model_path = 'titanet_large'
-        config2.diarizer.vad.external_vad_manifest = \
-            os.path.join(ROOT, 'info/configs/diarization_nemo/manifests/', 'external_vad_manifest.json')
-        config2.diarizer.out_dir = output_dir_2
-        config2.num_workers = 0
-        asr_ts_decoder = ASR_TIMESTAMPS(**config2.diarizer)
-        asr_model = asr_ts_decoder.set_asr_model()
-        word_hyp, word_ts_hyp = asr_ts_decoder.run_ASR(asr_model)
-        print(word_hyp)
-        print(word_ts_hyp)
+    config2 = OmegaConf.load(MODEL_CONFIG)
+    config2.diarizer.asr.model_path = 'QuartzNet15x5Base-En'
+    config2.diarizer.manifest_filepath = \
+        os.path.join(data_dir, 'manifests/', 'input_manifest.json')
+    config2.diarizer.speaker_embeddings.model_path = 'titanet_large'
+    config2.diarizer.vad.external_vad_manifest = \
+        os.path.join(data_dir, 'manifests/', 'external_vad_manifest.json')
+    config2.diarizer.out_dir = output_dir
+    config2.num_workers = 0
+    asr_ts_decoder = ASR_TIMESTAMPS(**config2.diarizer)
+    asr_model = asr_ts_decoder.set_asr_model()
+    word_hyp, word_ts_hyp = asr_ts_decoder.run_ASR(asr_model)
+    print(word_hyp)
+    print(word_ts_hyp)
 
-        asr_diar_offline = ASR_DIAR_OFFLINE(**config2.diarizer)
-        asr_diar_offline.word_ts_anchor_offset = asr_ts_decoder.word_ts_anchor_offset
-        diar_hyp, diar_score = asr_diar_offline.run_diarization(config2, word_ts_hyp)
-        print("Diarization hypothesis output: \n", diar_hyp)
-        result = asr_diar_offline.get_transcript_with_speaker_labels(diar_hyp, word_hyp, word_ts_hyp)
-        print(result)
-    else:
-        config = OmegaConf.load(MODEL_CONFIG)
-        config.diarizer.vad.external_vad_manifest = \
-            os.path.join(ROOT, 'info/configs/diarization_nemo/manifests/', 'external_vad_manifest.json')
-        pretrained_speaker_model = 'titanet_large'
-        config.diarizer.speaker_embeddings.model_path = pretrained_speaker_model
-        config.diarizer.manifest_filepath = \
-            os.path.join(ROOT, 'info/configs/diarization_nemo/manifests/', 'input_manifest.json')
-        config.diarizer.out_dir = output_dir
-        config.diarizer.speaker_embeddings.model_path = pretrained_speaker_model
-        config.diarizer.speaker_embeddings.parameters.window_length_in_sec = 1.5
-        config.diarizer.speaker_embeddings.parameters.shift_length_in_sec = 0.75
-        config.diarizer.oracle_vad = False
-        config.diarizer.clustering.parameters.oracle_num_speakers = True
-        config.num_workers = 0
-        sd_model = ClusteringDiarizer(cfg=config)
-        result = sd_model.diarize()
+    asr_diar_offline = ASR_DIAR_OFFLINE(**config2.diarizer)
+    asr_diar_offline.word_ts_anchor_offset = asr_ts_decoder.word_ts_anchor_offset
+    diar_hyp, diar_score = asr_diar_offline.run_diarization(config2, word_ts_hyp)
+    print("Diarization hypothesis output: \n", diar_hyp)
+    result = asr_diar_offline.get_transcript_with_speaker_labels(diar_hyp, word_hyp, word_ts_hyp)
+    file_to_show = os.path.join(data_dir, 'transcripts/pred_rttms/', file_path.split('/')[-1].split(".")[0], '.txt')
+    print(file_to_show)
+    print(diar_hyp)
     return result
